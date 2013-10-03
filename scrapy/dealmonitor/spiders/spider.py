bb8985a9cdf499d0e1e978943396e5fb56ba971a
tdubourg <plop@plop.fake>
2013-10-03 19:27:14 +0200

    First prototype of a spider.

--- scrapy/dealmonitor/spiders/spider.py
+++ scrapy/dealmonitor/spiders/spider.py
@@ -1,23 +1,32 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+from dealmonitor.items import *
+
+DBG = True
 
 class LBCSpider(BaseSpider):
     name = "lbc"
     allowed_domains = ["leboncoin.fr"]
     start_urls = [
-        "http://www.leboncoin.fr/annonces/offres/rhone_alpes/occasions/?f=a&th=1&q=brosse+a+dent&location=Paris%2075001"
+      "http://www.leboncoin.fr/annonces/offres/rhone_alpes/occasions/?f=a&th=1&q=brosse+a+dent&location=Paris%2075001"
     ]
 
     def parse(self, response):
-       hxs = HtmlXPathSelector(response)
-       sites = hxs.select('//div[@id="ContainerMain"]/div[@class="list-lbc"]')
-       items = []
-       for site in sites:
-           item = DealmonitorItem()
-           item['url'] = site.select('a/@href').extract()
-           item['title'] = site.select('a/@title').extract()
-           item['desc'] = "Placeholder" #site.select('text()').extract()
-           item['price'] = site.select('a/div[@class="price"]').extract()
-           item['date'] = site.select('a/div[@class="date"]').extract()
-           items.append(item)
-       return items
+        if DBG:
+            print "Execution with response=", response
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//div[@class="list-lbc"]/a')
+        # sites = hxs.select('//div[@id="ContainerMain"]/div[@class="list-lbc"]')
+        items = []
+        if DBG:
+            print "Entering foreach for:", sites
+        for site in sites:
+            print "\n\n\nSite!", site
+            item = DealmonitorItem()
+            item['url'] = site.select('@href').extract()
+            item['title'] = site.select('@title').extract()
+            item['desc'] = "Placeholder" #site.select('text()').extract()
+            item['date'] = site.select('//div[@class="date"]/div[position() = 1]/text()').extract()[0] + " " + site.select('//div[@class="date"]/div[position() = 2]/text()').extract()[0]
+            item['price'] = site.select('//div[@class="price"]/text()').extract()
+            items.append(item)
+        return items
