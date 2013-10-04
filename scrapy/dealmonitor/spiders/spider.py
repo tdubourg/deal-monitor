33742b6926749eafcfed9dfa5c8a3ac18b77c057
tdubourg <plop@plop.fake>
2013-10-04 11:17:18 +0200

    A first simple working spider for LBC

--- scrapy/dealmonitor/spiders/spider.py
+++ scrapy/dealmonitor/spiders/spider.py
@@ -1,6 +1,10 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from dealmonitor.items import *
+from BeautifulSoup import BeautifulSoup
+import re 
+
+price_regexp = re.compile(".*?(([0-9]+( [0-9]+)?)+)", re.MULTILINE | re.DOTALL)
 
 DBG = True
 
@@ -8,7 +12,8 @@ class LBCSpider(BaseSpider):
     name = "lbc"
     allowed_domains = ["leboncoin.fr"]
     start_urls = [
-      "http://www.leboncoin.fr/annonces/offres/rhone_alpes/occasions/?f=a&th=1&q=brosse+a+dent&location=Paris%2075001"
+      "http://www.leboncoin.fr/annonces/offres/rhone_alpes/occasions/?f=a&th=1&q=brosse+a+dent&location=Paris%2075001",
+      "http://www.leboncoin.fr/informatique/offres/rhone_alpes/?f=a&th=1&q=g500"
     ]
 
     def parse(self, response):
@@ -24,9 +29,19 @@ class LBCSpider(BaseSpider):
             print "\n\n\nSite!", site
             item = DealmonitorItem()
             item['url'] = site.select('@href').extract()
-            item['title'] = site.select('@title').extract()
+            item['title'] = site.select('@title').extract()[0].encode('utf-8')
             item['desc'] = "Placeholder" #site.select('text()').extract()
-            item['date'] = site.select('//div[@class="date"]/div[position() = 1]/text()').extract()[0] + " " + site.select('//div[@class="date"]/div[position() = 2]/text()').extract()[0]
-            item['price'] = site.select('//div[@class="price"]/text()').extract()
+            date1 = BeautifulSoup(site.select('.//div[@class="date"]/div[position() = 1]/text()').extract()[0].encode('utf-8')).string
+            date2 = BeautifulSoup(site.select('.//div[@class="date"]/div[position() = 2]/text()').extract()[0].encode('utf-8')).string
+            item['date'] = date1 + " " + date2
+            price_l = site.select('.//div[@class="price"]/text()').extract()
+            raw_price = price_l[0] if len(price_l) > 0 else ""
+            # print "raw_price=", raw_price
+            m = price_regexp.match(raw_price)
+            # print "Match=", m.groups()
+            try:
+                item['price'] = int(m.group(1).replace(" ", "")) if m is not None else -1
+            except ValueError:
+                item['price'] = -1
             items.append(item)
         return items
