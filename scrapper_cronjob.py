9e0fe63dde10a7dab2149a495a15600f5f2fce16
tdubourg <plop@plop.fake>
2013-10-14 17:02:08 +0200

    WIP On the logic after the crawl. Using items new id attributes. We are now able to compare them between runs. Updating them. We are now storing the history of sent alerts in order to know whether we should send an alert when a previously crawled item has changed (as, now, we can compare them, using their id). Still TODO send the actual alert

--- scrapper_cronjob.py
+++ scrapper_cronjob.py
@@ -4,6 +4,24 @@ DBG = True
 from utils.shell_utils import execute_shell_and_get_stdout
 import json
 import filter as f
+from bisect import bisect_left as bisect_search
+
+passed_alerts = dict([(int(id), o) for id,o in json.load(open("passed_alerts.json")).items()]) #TODO: Sorts prices lists so that we can use bisect_search()?
+
+def already_alerted(self, item):
+	"""
+	Tells whether we already sent an alert for this item, with its current price.
+	We consider we have the right to send an alert again is the price changed compared to the previous alert sent for this item.
+	"""
+	global passed_alerts
+	try:
+		item_passed_alerts = passed_alerts[item["id"]]
+		if item["price"] in item_passed_alerts:
+			return True
+		else:
+			return False
+	except KeyError:
+		return False
 
 # Launch scrapy job
 
@@ -11,6 +29,8 @@ import filter as f
 # Load items
 items_lbc_file = open("items_lbc.json")
 items = json.load(items_lbc_file)
+
+already_existing_items = dict([(int(o["id"], o) for o in json.load(open("filtered_items.json"))])
 
 # Load filters
 filters_file = open('filters.json')
@@ -23,7 +43,19 @@ filters = [f.fromJSON(json_data) for json_data in filters_json_data]
 if DBG:
 	print "Filters!!", filters
 
+output_items = {}
+for f in filters:
+	output_items[f.name] = []
+
+
 for item in items:
+	item["id"] = item_id = int(item["id"])
+	
+	try:
+		existing_item = already_existing_items[item_id]
+	except KeyError:
+		existing_item = None
+
 	if DBG:
 		print "Item=", item
 	for f in filters:
@@ -32,10 +64,17 @@ for item in items:
 		if f.satisfies(item):
 			if DBG:
 				print "Item satisfies filter", f
-			# Insert in db
-			#TODO
-		if f.satisfies_alert(item):
-			if DBG:
-				print "Item satisfies alert for filter", f
-			# Send alert
-			#TODO
+			if existing_item is not None: # This item was already inserted
+				 # Did it change?
+				 if existing_item != item:
+				 	# Then update it
+				 	already_existing_items[item["id"]] = item
+			else:
+				# New satisfying item, insert in db
+				output_items[f.name].append(item)
+
+			if f.satisfies_alert(item) and not already_alerted(item):
+				if DBG:
+					print "Item satisfies alert for filter", f
+				# Send alert
+				#TODO
