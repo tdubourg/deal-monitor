3c2d20bae2a23f543857c8d80b9d5edf563ceb9e
tdubourg <plop@plop.fake>
2013-10-14 12:04:13 +0200

    Fixing issue where min/max prices would be both mandatory where they should be optional

--- filter.py
+++ filter.py
@@ -33,19 +33,24 @@ class Filter(object):
 		self.data = data
 	
 	def satisfies(self, item):
-		if ((self.data["min_price"] is not None and item["price"] >= self.data["min_price"]) and
-			(self.data["max_price"] is not None and item["price"] <= self.data["max_price"])):
+		if ((self.data["min_price"] is None or item["price"] >= self.data["min_price"]) and
+			(self.data["max_price"] is None or item["price"] <= self.data["max_price"])):
 			return True
 		return False
 
 	def __repr__(self):
-		return "(min=%s, max=%s, min_al=%s, max_al=%s)" % (self.data["min_price"], self.data["max_price"], self.data["min_price_for_alert"], self.data["max_price_for_alert"])
+		return "(min=%s, max=%s, min_al=%s, max_al=%s)" % (
+			self.data["min_price"],
+			self.data["max_price"],
+			self.data["min_price_for_alert"],
+			self.data["max_price_for_alert"]
+		)
 
 	def satisfies_alert(self, item):
 		if not self.satisfies(item):
 			return False
 
-		if ((self.data["min_price_for_alert"] is not None and item["price"] >= self.data["min_price_for_alert"]) and
-			(self.data["max_price_for_alert"] is not None and item["price"] <= self.data["max_price_for_alert"])):
+		if ((self.data["min_price_for_alert"] is None or item["price"] >= self.data["min_price_for_alert"]) and
+			(self.data["max_price_for_alert"] is None or item["price"] <= self.data["max_price_for_alert"])):
 			return True
 		return False
