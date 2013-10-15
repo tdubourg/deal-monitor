d7ed9fdc663c57f89b997d88aca67889b346a6f4
tdubourg <plop@plop.fake>
2013-10-15 11:14:40 +0200

    Now persisted filtered items and alerts already sent so that we do not send them again (unless price changed)

--- scrapper_cronjob.py
+++ scrapper_cronjob.py
@@ -7,6 +7,10 @@ import filter as f
 from bisect import bisect_left as bisect_search
 import smtplib
 
+ITEMS_FILEPATH = "items_lbc.json"
+FILTERED_ITEMS_FILEPATH = "filtered_items.json"
+PASSED_ALERTS_FILEPATH = "passed_alerts.json"
+
 def send_alert(item, filter):
 	to = 'trolldu26@gmail.com'
 	gmail_user = 'notimportant2014@gmail.com'
@@ -27,7 +31,22 @@ def send_alert(item, filter):
 	sc.sendmail(gmail_user, to, msg)
 	sc.close()
 
-passed_alerts = dict([(int(id), o) for id,o in json.load(open("passed_alerts.json")).items()]) #TODO: Sort prices lists so that we can use bisect_search()?
+	# Now, register this sent alert in the passed alerts:
+	register_alerted(item)
+
+def register_alerted(item):
+	global passed_alerts
+	try:
+		item_passed_alerts = passed_alerts[item["id"]]
+	except KeyError:
+		item_passed_alerts = []
+		passed_alerts[item["id"]] = item_passed_alerts
+
+	item_passed_alerts.append(item["price"])
+
+f_passed_alerts = open(PASSED_ALERTS_FILEPATH, 'r')
+passed_alerts = dict([(int(id), o) for id,o in json.load(f_passed_alerts).items()]) #TODO: Sort prices lists so that we can use bisect_search()?
+f_passed_alerts.close()
 
 def already_alerted(item):
 	"""
@@ -48,14 +67,16 @@ def already_alerted(item):
 
 # Analyze results, store the ones that fulfill the filters, send alerts for the ones that fulfill alert filters
 # Load items
-items_lbc_file = open("items_lbc.json")
+items_lbc_file = open(ITEMS_FILEPATH)
 items = json.load(items_lbc_file)
+items_lbc_file.close()
 
-already_existing_items = dict([(int(o["id"]), o) for o in json.load(open("filtered_items.json"))])
+already_existing_items = dict([(int(o["id"]), o) for o in json.load(open(FILTERED_ITEMS_FILEPATH))])
 
 # Load filters
 filters_file = open('filters.json')
 filters_json_data = json.load(filters_file)
+filters_file.close()
 if DBG:
 	print "Filters data:", filters_json_data
 
@@ -68,6 +89,7 @@ output_items = {}
 for f in filters:
 	output_items[f.name] = []
 
+new_items = []
 
 for item in items:
 	item["id"] = item_id = int(item["id"])
@@ -92,7 +114,8 @@ for item in items:
 				 	already_existing_items[item["id"]] = item
 			else:
 				# New satisfying item, insert in db
-				output_items[f.name].append(item)
+				# output_items[f.name].append(item)
+				new_items.append(item) # TODO improve that to insert by filter, as the commented line above
 
 			if f.satisfies_alert(item):
 				if DBG:
@@ -103,4 +126,18 @@ for item in items:
 				else:
 					if DBG:
 						print "Item satisfies alert for filter but already alerted", f
+
+f_existing_items = open(FILTERED_ITEMS_FILEPATH, 'w+')
+total_items = already_existing_items.values() + new_items
+
+# While debugging, use pretty-printed JSON, when not debugging anymore, use compact notation for space efficiency
+if DBG:
+	json_repr = json.dumps(total_items, indent=4, separators=(',', ': '))
+else:
+	json_repr = json.dumps(total_items)
+f_existing_items.write(json_repr)
+f_existing_items.close()
 				
+f_passed_alerts = open(PASSED_ALERTS_FILEPATH, 'w+')
+json.dump(passed_alerts, f_passed_alerts)
+f_passed_alerts.close()
