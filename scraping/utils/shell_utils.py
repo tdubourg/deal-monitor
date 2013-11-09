373a18fa2044aa8d7917e386cf2ab96e90a2471d
tdubourg <plop@plop.fake>
2013-11-09 14:54:24 +0100

    Adding a variant where we grab stderr too

--- scraping/utils/shell_utils.py
+++ scraping/utils/shell_utils.py
@@ -1,6 +1,6 @@
 from subprocess import Popen, PIPE
 
-DBG = False
+DBG = True
 
 
 def execute_shell_and_get_stdout(shell_command, args_list):
@@ -9,3 +9,10 @@ def execute_shell_and_get_stdout(shell_command, args_list):
     command = [shell_command]
     command.extend(args_list)
     return Popen(command, stdout=PIPE, stderr=PIPE).communicate()[0] # [0] is stdout, [1] is stderr
+
+def execute_shell_and_get_stdout_and_err(shell_command, args_list):
+    if DBG:
+        print "Going to execute command: ", shell_command, args_list
+    command = [shell_command]
+    command.extend(args_list)
+    return Popen(command, stdout=PIPE, stderr=PIPE).communicate()
