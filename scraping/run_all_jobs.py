d8a22938ca887bb2a32ebf98279d8f35ff04b201
tdubourg <plop@plop.fake>
2013-11-09 14:55:30 +0100

    Fixed bugs (missing parameters, wrong paths...) + added some logging

--- scraping/run_all_jobs.py
+++ scraping/run_all_jobs.py
@@ -3,24 +3,40 @@
 import json
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../")))
-from utils.shell_utils import execute_shell_and_get_stdout as execsh
+from utils.shell_utils import execute_shell_and_get_stdout_and_err as execsh
 
 DATA_PATH = "data/"
 JOBS_FILEPATH = DATA_PATH + "jobs.json"
+DBG = True
 
 if __name__ == '__main__':
     f_jobs = open(JOBS_FILEPATH, "r")
     jobs = json.load(f_jobs)
     f_jobs.close()
-    for j in jobs:
+    if DBG:
+        print "Loaded %s jobs" % len(jobs)
+        print jobs
+
+    for j in jobs.values():
+        if DBG:
+            print "Job %s..." % j["job_name"]
         # Scrap the data...
-        execsh(
+        items_file_path = DATA_PATH + j["job_name"] + "/items.json"
+        os.remove(DATA_PATH + j["job_name"] + "/items.json")
+        res = execsh(
             "./scrap",
             [
-                  "-o", DATA_PATH + j.name + "/items.json"
-                , "-a", 'region="%s"' % j.region
-                , "-a", 'keywords="%s"' % j.keywords
+                  "-o", "../" + items_file_path
+                , "-a", 'region=%s' % j["region"]
+                , "-a", 'keywords=%s' % j["keywords"]
+                , "-a", 'category=%s' % j["category"]
             ]
         )
+        if DBG:
+            print "Result of scrapy execution:"
+            print res
         # Process the data...
-        execsh("./process_data_for_job.py", [j.name])
+        res = execsh("./process_data_for_job.py", [j["job_name"]])
+        if DBG:
+            print "Result of job processing execution:"
+            print res
